4761e2e7762219a2232a7318a4ce0706aef11924
yoddi <yoddi.dahsyat@gmail.com>
2025-04-15 08:56:27 +0700

    fix: cta button link and wording

--- src/components/homepage/cta.tsx
+++ src/components/homepage/cta.tsx
@@ -1,17 +1,39 @@
-import Link from 'next/link';
+import UnderlineLink from '@/components/links/UnderlineLink';
 
 const CTA = () => {
   return (
-    <section className='h-screen container mx-auto text-4xl grid grid-rows-5'>
-      <div className='row-start-3 flex flex-col gap-8'>
-        <h1 className='font-light'>Reach me out on</h1>
-        <Link href='mailto:yoddi.dahsyat@gmail.com' className='font-light'>
-          yoddi.dahsyat@gmail.com
-        </Link>
+    <section className='h-screen container mx-auto relative grid grid-rows-6'>
+      <div className='row-start-4'>
+        <h1 className='font-light text-4xl'>
+          Ready to bring your next idea to life?
+        </h1>
+        <p className='font-light text-2xl'>
+          Say hello at{' '}
+          <UnderlineLink
+            href='mailto:yoddi.dahsyat@gmail.com'
+            className='font-light mt-4'
+          >
+            yoddi.dahsyat@gmail.com
+          </UnderlineLink>
+        </p>
       </div>
-      <div className='row-start-5 flex gap-12 font-light'>
-        <Link href='https://github.com/yoddidahsyat'>github</Link>
-        <Link href='https://www.linkedin.com/in/yoddidahsyat'>linkedin</Link>
+      <div className='flex gap-12 row-start-6 text-2xl'>
+        <div>
+          <UnderlineLink
+            href='https://github.com/yoddidahsyat'
+            className='font-light'
+          >
+            github
+          </UnderlineLink>
+        </div>
+        <div>
+          <UnderlineLink
+            href='https://www.linkedin.com/in/yoddidahsyat'
+            className='font-light'
+          >
+            linkedin
+          </UnderlineLink>
+        </div>
       </div>
     </section>
   );
