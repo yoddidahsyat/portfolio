d6fc95232392c6d714f345afa56875c0ed74e01a
yoddi <yoddi.dahsyat@gmail.com>
2025-08-30 18:45:51 +0700

    fix: summary layout responsive

--- src/components/homepage/projects.tsx
+++ src/components/homepage/projects.tsx
@@ -6,10 +6,8 @@ const Projects = () => {
   return (
     <section className='min-h-screen' id='projects'>
       <div className='container mx-auto py-16 grid grid-cols-1 sm:grid-cols-3'>
-        <div className='col flex justify-start'>
-          <h1 className='font-light sticky bottom-16 mt-auto text-4xl'>
-            My Projects
-          </h1>
+        <div className='col sticky bottom-16 mt-auto space-y-4'>
+          <h1 className='font-light text-4xl'>Selected Projects</h1>
         </div>
         <div className='sm:col-span-2 grid gap-4'>
           {projects.map((project) => (
--- src/components/homepage/summary.tsx
+++ src/components/homepage/summary.tsx
@@ -24,41 +24,43 @@ const Summary = () => {
   return (
     <section className='h-screen scroll-smooth' id='about'>
       <div className='container h-full mx-auto py-16 grid grid-cols-3'>
-        <div className='grid grid-rows-6 sm:col-span-1 col-span-2'>
-          <h2 className='sm:row-span-3 row-span-2 text-2xl sm:text-4xl font-light leading-normal'>
+        <div className='lg:col-span-1 col-span-3 flex justify-between flex-col'>
+          <h2 className='text-2xl sm:text-4xl font-light leading-normal'>
             learn, code, and bring{' '}
             <span className='text-primary-500'>impact</span> to the world.
           </h2>
-          <p>
-            My name is Mohammad Yoddi Dahsyat, a Full-Stack Developer based in
-            Indonesia. My main stacks are Next.js and Node.js, but I also have
-            experience in PHP and Laravel.
-          </p>
-          <p>
-            Currently working at{' '}
-            <UnderlineLink
-              href='https://vaforeveryone.com.au'
-              className='text-primary-500 text-base border-none font-normal'
-              openNewTab
-            >
-              VA For Everyone
-            </UnderlineLink>
-            , a Virtual Assistant agency based in Australia. I usually handle
-            both frontend and backend of client's web application, with various
-            stacks and architecture.
-          </p>
-          <p>
-            Feel free to contact me on{' '}
-            <UnderlineLink
-              href='mailto:yoddidahsyat@gmail.com'
-              className='text-primary-500 text-base border-none font-normal'
-              openNewTab
-            >
-              yoddi.dahsyat@gmail.com
-            </UnderlineLink>
-          </p>
+          <div className='space-y-4'>
+            <p>
+              My name is Mohammad Yoddi Dahsyat, a Full-Stack Developer based in
+              Indonesia. My main stacks are Next.js and Node.js, but I also have
+              experience in PHP and Laravel.
+            </p>
+            <p>
+              Currently working at{' '}
+              <UnderlineLink
+                href='https://vaforeveryone.com.au'
+                className='text-primary-500 text-base border-none font-normal'
+                openNewTab
+              >
+                VA For Everyone
+              </UnderlineLink>
+              , a Virtual Assistant agency based in Australia. I usually handle
+              both frontend and backend of client's web application, with
+              various stacks and architecture.
+            </p>
+            <p>
+              Feel free to contact me on{' '}
+              <UnderlineLink
+                href='mailto:yoddidahsyat@gmail.com'
+                className='text-primary-500 text-base border-none font-normal'
+                openNewTab
+              >
+                yoddi.dahsyat@gmail.com
+              </UnderlineLink>
+            </p>
+          </div>
         </div>
-        <div className='sm:col-start-3 flex items-end'>
+        <div className='lg:col-start-3 col-span-3 flex items-end'>
           <div className='flex flex-wrap space-y-4 space-x-2 justify-center'>
             {stacks.map((stack) => (
               <Badge
